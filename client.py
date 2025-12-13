b721624a667ffadb54d012df03edf5342972a270
DucNguyenhuuminh <nguyenhuuminhduc2832005@gmail.com>
2025-12-13 15:53:51 +0700

    finish update client

--- client.py
+++ client.py
@@ -3,30 +3,61 @@ import json
 
 proxy = xmlrpc.client.ServerProxy("http://localhost:8000/MyServerRpc")
 
-try:
-    response = proxy.execute_http("https://jsonplaceholder.typicode.com/todos/1", "GET", {}, "")
-    print("GET Status:", response['status_code'])
-except Exception as e:
-    print("GET REQUEST ERROR:", e)
+def get_valid_input(prompt, func):  
+    while True:
+        user_input = input(prompt).strip()
+        try:
+            return func(user_input)
+        except ValueError as e:
+            print(f"Error: {e}. Try again")
 
-print("Please enter data to send to the server: ")
-user_name = input("Enter name: ")
-age = input("Enter age:")
-user_major = input("Enter major:")
+def check_url(text):
+    if not text.startswith("http"):
+        raise ValueError("URL must start with http or https")
+    return text
 
-my_mess = {
-    'name': user_name,
-    'age':age,
-    'job': user_major
-}
+def check_method(text):
+    result = text.upper()
+    valid_methods = {"GET","POST","PUT","DELETE"}
+    if result not in valid_methods:
+        raise ValueError(f"Method must be one of: {valid_methods}")
+    return result
 
-convert_2_jsStr = json.dumps(my_mess)
-header = {'Content-type': 'application/json'}
+def check_header(text):
+    if text == "":
+        return {}
+    
+    try:
+        data = json.loads(text)
+    except json.JSONDecodeError:
+        raise ValueError("Header must have a valid JSON format")
+    
+    if not isinstance(data,dict):
+        raise ValueError("Header must be a dict type")
+    return data  
 
-try:
-    test = proxy.execute_http("https://jsonplaceholder.typicode.com/posts", "POST", header, convert_2_jsStr)
+def client_main():
+    print("------ PREPARING REQUEST ------\n")
+    url = get_valid_input("Enter URL: ",check_url)
+    method = get_valid_input("Enter method: ",check_method)
+    header = get_valid_input("Enter header: ",check_header)
+    body = ""
+    if method in ["POST","PUT"]:
+        body = input("Enter body load: ").strip()
+    else:
+        print(f"Skip body for {method}")
 
-    print("Status for POST method : ", test['status_code'])
-    print("Response content:", test['content'])
-except Exception as e:
-    print("POST REQUEST ERROR: ", e)
+    print(f"Sending {method} request to server...........")
+
+    try:
+        response = proxy.execute_http(url,method,header,body)
+
+        print("\n------ RESPONSE FROM SERVER ------")
+        print(f"Status: {response.get('status_code')}")
+        print(f"Content: {response.get('content')}")
+        print('------ FINISH RESPONSE ------')
+    except Exception as e:
+        print(f"Connection error: {e}")
+
+if __name__ == "__main__":
+    client_main()
